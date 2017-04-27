d674c80a8dfc530c38b20c1dd874a83681444cf4
h6gs <harrystirling94@gmail.com>
2017-04-27 14:34:45 +0100

    fixed button position on home

--- src/js/client.js
+++ src/js/client.js
@@ -27,6 +27,7 @@ function init() {
     $(`#${scoreCounter}score`).text(score);
     $.post(`${window.location.origin}/media/${scoreCounter}`, { score });
   });
+
   $('.carousel').carousel();
   autoplay();
   function autoplay() {
